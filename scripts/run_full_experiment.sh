ee2e6ac3c073dde050b2e32114dd095ea0fdd2d2
Jay Wang <51730216+withwangjie@users.noreply.github.com>
2026-09-25 09:36:05 +0800

    Let QP_VENV override server-config venv in shell entrypoint

--- scripts/run_full_experiment.sh
+++ scripts/run_full_experiment.sh
@@ -50,6 +50,9 @@ server_venv_hint() {
     }' "$SERVER_CONFIG"
 }
 SERVER_VENV_HINT="$(server_venv_hint)"
+if [ -n "${QP_VENV:-}" ]; then
+    SERVER_VENV_HINT=""
+fi
 if [ -n "$SERVER_VENV_HINT" ]; then
     case "$SERVER_VENV_HINT" in
         "~/"*) SERVER_VENV_HINT="${HOME}/${SERVER_VENV_HINT#~/}" ;;
